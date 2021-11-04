ce3e12883a15caae88d6888a0b15b138847c2cc6
mapacarta <omergunduz123@hotmail.com>
2021-11-04 08:25:49 +0300

    fixed

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-var unbrotli = require('./unbrotli');
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.raw = undefined;
+exports.default = loader;
 
 
-function decompress(e,n){return unbrotli(_base64ToArrayBuffer(e))}
+var _mime = require('mime');
 
-function _base64ToArrayBuffer(e){
-    for(var n=window.atob(e),r=n.length,o=new Uint8Array(r),t=0;t<r;t++)o[t]=n.charCodeAt(t);return o
+var _mime2 = _interopRequireDefault(_mime);
+
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Loader Mode
+const raw = exports.raw = true; /* eslint-disable
+                                  global-require,
+                                  no-param-reassign,
+                                  prefer-destructuring,
+                                  import/no-dynamic-require,
+                                */
+function loader(src) {
+  // Loader Options  
+
+  const file = this.resourcePath;
+  const mimetype = _mime2.default.getType(file);
+  
+  if (typeof src === 'string') {
+    src = Buffer.from(src);
+  }
+
+  let base122 = require('./base122');
+  let encoded = Buffer.from(base122.encode(src), 'utf8');  
+
+  let final = '___base122___' + encoded.toString('utf-8');
+
+  return `module.exports = "${final}"`;
+  
 }
-
-
-function decompressString(r){
-    return new Promise(function(e,n){
-        e(new TextDecoder("utf-8").decode(decompress(r)))
-    })
-}
-
-
-var rawJs = require('../../../build/htmls/brotli/main.br');
-rawJs = rawJs.slice(13);
-
-
-//var rawJs = require('!!url-loader!../br/luna.br');
-
-
-window._compressedAssets = window._compressedAssets || []; window._compressedAssets.push( 
-    decompressString(rawJs)
-    .then( function( code ) { window.eval(code) } ) );
-
-function callStart(){
-    if(!window.app || !window.app.callOnLoad){
-        setTimeout(callStart, 100);
-        return;
-    }
-
-    app.callOnLoad();
-};
-
-window.addEventListener("load", function(){
-    callStart();
-})
